5dc8dfd9740c28ef33d65e36665343a38a18d84c
Eric Yan <github@ericyan.me>
2020-08-05 21:42:35 +0200

    Add support for gcast device discovery

--- gcast/device.go
+++ gcast/device.go
@@ -1,15 +1,24 @@
 package gcast
 
 import (
+	"context"
 	"encoding/json"
 	"io/ioutil"
 	"net"
 	"net/http"
 	"net/url"
+	"strings"
+
+	"github.com/grandcat/zeroconf"
 )
 
 type DeviceInfo struct {
-	Name string `json:"name"`
+	Name  string `json:"name"`
+	Model string
+
+	IPv4 net.IP
+	IPv6 net.IP
+	Port int
 }
 
 func GetDeviceInfo(ip net.IP) (*DeviceInfo, error) {
@@ -35,4 +44,62 @@ func GetDeviceInfo(ip net.IP) (*DeviceInfo, error) {
 	}
 
 	return &info, nil
+}
+
+// Discover returns a channel with DeviceInfo found via mDNS.
+func Discover(ctx context.Context) (<-chan *DeviceInfo, error) {
+	resolv, err := zeroconf.NewResolver()
+	if err != nil {
+		return nil, err
+	}
+
+	mdnsCh := make(chan *zeroconf.ServiceEntry)
+	go func() {
+		if err := resolv.Browse(ctx, "_googlecast._tcp", "local", mdnsCh); err != nil {
+			return
+		}
+	}()
+
+	devCh := make(chan *DeviceInfo)
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				close(devCh)
+				return
+			case mdns := <-mdnsCh:
+				if mdns == nil {
+					continue
+				}
+
+				dev := new(DeviceInfo)
+
+				if len(mdns.AddrIPv4) > 0 {
+					dev.IPv4 = mdns.AddrIPv4[0]
+				}
+				if len(mdns.AddrIPv6) > 0 {
+					dev.IPv6 = mdns.AddrIPv6[0]
+				}
+
+				dev.Port = mdns.Port
+
+				for _, value := range mdns.Text {
+					if kv := strings.SplitN(value, "=", 2); len(kv) == 2 {
+						key, val := kv[0], kv[1]
+
+						switch key {
+						case "fn":
+							dev.Name = val
+						case "md":
+							dev.Model = val
+						}
+					}
+				}
+
+				devCh <- dev
+			}
+		}
+	}()
+
+	return devCh, nil
 }
