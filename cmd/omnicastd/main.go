1f10ccc1f0e5fc9e09be6e40fe156353bf06a1a6
Eric Yan <github@ericyan.me>
2020-08-14 21:17:25 +0200

    Enable discovery for MPRIS players

--- cmd/omnicastd/main.go
+++ cmd/omnicastd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"log"
 	"os"
@@ -14,6 +15,7 @@ import (
 
 	"github.com/ericyan/omnicast"
 	"github.com/ericyan/omnicast/gcast"
+	"github.com/ericyan/omnicast/mpris"
 	"github.com/ericyan/omnicast/upnp"
 	"github.com/ericyan/omnicast/upnp/av"
 )
@@ -24,6 +26,43 @@ func init() {
 	if addr, _ := iputil.DefaultIPv4(); addr != nil {
 		defaultHost = addr.IP.String()
 	}
+}
+
+func gcastPlayer() (*gcast.Sender, error) {
+	ctx, stopDiscovery := context.WithTimeout(context.Background(), time.Duration(10)*time.Second)
+	ch, err := gcast.Discover(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	for dev := range ch {
+		if dev.CapableOf(gcast.VideoOut, gcast.AudioOut) {
+			log.Printf("Found Google Cast device: %s (%s)\n", dev.Name, dev.UUID)
+
+			sender, err := gcast.NewSender("sender-omnicast", dev)
+			if err != nil {
+				return nil, err
+			}
+
+			stopDiscovery()
+			return sender, nil
+		}
+	}
+
+	return nil, errors.New("no Google Cast device found")
+}
+
+func mprisPlayer() (*mpris.Player, error) {
+	dests, err := mpris.Discover()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, dest := range dests {
+		return mpris.NewPlayer(dest)
+	}
+
+	return nil, errors.New("no MPRIS player found")
 }
 
 func main() {
@@ -37,25 +76,16 @@ func main() {
 		os.Exit(0)
 	}
 
-	ctx, stopDiscover := context.WithTimeout(context.Background(), time.Duration(10)*time.Second)
-
-	ch, err := gcast.Discover(ctx)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
 	var player omnicast.MediaPlayer
-	for dev := range ch {
-		if dev.CapableOf(gcast.VideoOut, gcast.AudioOut) {
-			log.Printf("Found Google Cast device: %s (%s)\n", dev.Name, dev.UUID)
 
-			sender, err := gcast.NewSender("sender-omnicast", dev)
-			if err != nil {
-				log.Fatalln(err)
-			}
+	player, err := gcastPlayer()
+	if err != nil {
+		log.Println(err)
 
-			player = sender
-			stopDiscover()
+		// Fallback to MPRIS
+		player, err = mprisPlayer()
+		if err != nil {
+			log.Fatalln(err)
 		}
 	}
 
